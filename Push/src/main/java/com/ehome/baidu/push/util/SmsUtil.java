cffd856768f182a3240beef9198821f0315e9a35
haoxiaolei <xiaolei-0228@163.com>
2015-12-12 10:55:23 +0800

    推送

--- Push/src/main/java/com/ehome/baidu/push/util/SmsUtil.java
+++ Push/src/main/java/com/ehome/baidu/push/util/SmsUtil.java
@@ -30,7 +30,9 @@ public class SmsUtil {
         }
 
         HttpClient httpClient = new HttpClient();
-        
+        PostMethod postMethod = new PostMethod("http://203.81.21.34/send/gsend.asp");
+        postMethod.setParameter("name", "zzncp");
+        postMethod.setParameter("pwd", "hubo721521");
         postMethod.setParameter("dst", mobile);
         postMethod.setParameter("longmsg", "1");
         postMethod.setParameter("sno", "");
