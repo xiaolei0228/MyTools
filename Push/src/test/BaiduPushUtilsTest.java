0ade2d9a5b599ff748795d84d6e5c78232d677ae
xiaolei-0228@163.com <xiaolei-0228@163.com>
2016-08-16 10:01:54 +0800

    推送

---
+++ Push/src/test/BaiduPushUtilsTest.java
@@ -0,0 +1,27 @@
+import com.baidu.yun.push.exception.PushClientException;
+import com.baidu.yun.push.exception.PushServerException;
+import com.ehome.baidu.push.util.BaiduPushUtil;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * MyTools
+ *
+ * @author: haoxiaolei
+ * @date: 2016-08-16 09:53
+ * @desc: 百度推送测试类
+ */
+public class BaiduPushUtilsTest {
+
+    @Test
+    public void push() throws PushClientException, PushServerException {
+        Map<String, String> pushMap = new HashMap<>();
+        pushMap.put("channelId", "3806776883274763272");
+        pushMap.put("title", "我是消息标题");
+        pushMap.put("description", "我是测试推送消息的内容......");
+
+        BaiduPushUtil.pushSingleDeviceMsg(pushMap);
+    }
+}
